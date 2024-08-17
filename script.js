5f94587e346ef05e6bbf59c71ccc759c224baab5
SABARI G <143690545+sabarig08@users.noreply.github.com>
2024-08-17 16:49:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+function updateClock() {
+    const timeElement = document.getElementById('time');
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    timeElement.textContent = timeString;
+}
+setInterval(updateClock, 1000);
+updateClock();
